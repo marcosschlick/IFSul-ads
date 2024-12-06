c16c3061f1c9c059e386fee49968d08eb9aeb267
Marcos Schlick <167476526+marcosschlick@users.noreply.github.com>
2024-12-05 23:16:50 -0300

    Add files via upload

--- projeto-final/bens/TelaBens.java
+++ projeto-final/bens/TelaBens.java
@@ -1,4 +1,5 @@
 package bens;
+
 import java.awt.CardLayout;
 import java.awt.Color;
 
@@ -16,20 +17,23 @@ public class TelaBens extends JPanel {
 
 		Title title = new Title("Bens");
 
-		NavigationButton vizualizarBens = new NavigationButton("Vizualizar", 100, 400, 300, 200);
+		NavigationButton visualizarBens = new NavigationButton("Visualizar", 100, 400, 300, 200);
 		NavigationButton procurarBem = new NavigationButton("Procurar", 450, 400, 300, 200);
 		NavigationButton novoBem = new NavigationButton("Criar", 800, 400, 300, 200);
 
-		
 		procurarBem.addActionListener(e -> cardLayout.show(cards, "TelaProcurarBem"));
 		novoBem.addActionListener(e -> cardLayout.show(cards, "TelaCriarBem"));
-		vizualizarBens.addActionListener(e -> cardLayout.show(cards, "TelaVizualizarBens"));
-		
-		
+
+		visualizarBens.addActionListener(e -> {
+			TelaVisualizarBens telaVisualizarBens = new TelaVisualizarBens(cardLayout, cards);
+			cards.add(telaVisualizarBens, "TelaVisualizarBens");
+			cardLayout.show(cards, "TelaVisualizarBens");
+		});
+
 		BackButton backButton = new BackButton(cardLayout, cards, "TelaPrincipal");
 
 		add(title);
-		add(vizualizarBens);
+		add(visualizarBens);
 		add(procurarBem);
 		add(novoBem);
 		add(backButton);
--- projeto-final/bens/TelaProcurarBem.java
+++ projeto-final/bens/TelaProcurarBem.java
@@ -3,12 +3,17 @@ package bens;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import commons.BackButton;
+import commons.ConnectionMySQL;
 import commons.NavigationButton;
 import commons.Title;
 
@@ -30,9 +35,11 @@ public class TelaProcurarBem extends JPanel {
 		pesquisaCaixa.setBounds(350, 300, 500, 70);
 		pesquisaCaixa.setFont(new Font("Arial", Font.BOLD, 20));
 
-		NavigationButton vizualizarMovimentacoes = new NavigationButton("Ver histórico", 100, 400, 300, 200);
-		NavigationButton moverBem = new NavigationButton("Mover bem", 450, 400, 300, 200);
-		NavigationButton baixarBem = new NavigationButton("Baixar bem", 800, 400, 300, 200);
+		NavigationButton vizualizarMovimentacoes = new NavigationButton("Ver", 50, 400, 200, 180);
+		NavigationButton moverBem = new NavigationButton("Mover", 275, 400, 200, 180);
+		NavigationButton baixarBem = new NavigationButton("Baixar", 500, 400, 200, 180);
+		NavigationButton editarBem = new NavigationButton("Editar", 725, 400, 200, 180);
+		NavigationButton deletarBem = new NavigationButton("Deletar", 950, 400, 200, 180);
 
 		vizualizarMovimentacoes.addActionListener(e -> {
 			int codigoMovel = Integer.parseInt(pesquisaCaixa.getText().trim());
@@ -55,8 +62,28 @@ public class TelaProcurarBem extends JPanel {
 			cardLayout.show(cards, "TelaMovimentacao");
 		});
 
+		editarBem.addActionListener(e -> {
+			int codigoMovel = Integer.parseInt(pesquisaCaixa.getText().trim());
+			TelaEditarBem telaEditarBem = new TelaEditarBem(cardLayout, cards, codigoMovel);
+			cards.add(telaEditarBem, "TelaEditarBem");
+			cardLayout.show(cards, "TelaEditarBem");
+		});
+
+		deletarBem.addActionListener(e -> {
+			int codigoMovel = Integer.parseInt(pesquisaCaixa.getText().trim());
+			try {
+				deletarBem(codigoMovel);
+				JOptionPane.showMessageDialog(null, "Móvel deletado com sucesso");
+			} catch (SQLException d) {
+				JOptionPane.showMessageDialog(null, "Ocorreu um erro: " + d.getMessage(), "Erro",
+						JOptionPane.ERROR_MESSAGE);
+			}
+		});
+
 		BackButton backButton = new BackButton(cardLayout, cards, "TelaBens");
 
+		add(editarBem);
+		add(deletarBem);
 		add(title);
 		add(pesquisaLegenda);
 		add(pesquisaCaixa);
@@ -65,5 +92,19 @@ public class TelaProcurarBem extends JPanel {
 		add(baixarBem);
 		add(backButton);
 
+	}
+
+	private void deletarBem(int codigoMovel) throws SQLException {
+		Connection connection = ConnectionMySQL.getConnection();
+		String sql = """
+				DELETE FROM Movel
+				WHERE CodigoPatrimonial = ?;
+				""";
+
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setInt(1, codigoMovel);
+		stmt.executeUpdate();
+		stmt.close();
+		connection.close();
 	}
 }
