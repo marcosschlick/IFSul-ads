b977015c37e6fed54b992c10e4f5ab4ad40a5837
Marcos Schlick <167476526+marcosschlick@users.noreply.github.com>
2024-12-12 17:12:37 -0300

    Update VerifyButton.java

--- Primeiros-jogos/jogo-da-senha/VerifyButton.java
+++ Primeiros-jogos/jogo-da-senha/VerifyButton.java
@@ -1,10 +1,7 @@
-package game;
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
